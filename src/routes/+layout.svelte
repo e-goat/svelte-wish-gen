ea03d9ebcd5fcc9c468e223247fb7062e4d6bd44
e-goat <gorgonaly@gmail.com>
2025-06-28 11:53:40 +0300

    Implement conditional rendering for production and development environments in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,26 @@
       import "../app.css";
 </script>
 
-<main class="px-4 md:px-8 mx-auto max-w-7xl">
-    <nav class="flex justify-between border border-amber-600">
-        <div>#logo</div>
-        <ul class="flex justify-center items-center gap-2">
-            <li>Home</li>
-            <li>About us</li>
-        </ul>
-    </nav>
-    <div class="py-6 md:py-10 flex justify-center items-start lg:items-center">
-        {@render children()}
-    </div>
-</main>
+{#if import.meta.env.PROD}
+    <main class="px-4 md:px-8 mx-auto max-w-7xl">
+        <div class="flex flex-col items-center justify-center min-h-screen">
+            <h1 class="text-3xl font-bold mb-4">Coming Soon</h1>
+            <p class="text-lg">
+                The app is coming soon. KartichkaQR is your favorite wish card generator—create personalized, beautiful cards for any occasion in seconds. Stay tuned for the easiest way to send heartfelt wishes!
+            </p>
+        </div>
+    </main>
+{:else}
+    <main class="px-4 md:px-8 mx-auto max-w-7xl">
+        <nav class="flex justify-between border border-amber-600">
+            <div>#logo</div>
+            <ul class="flex justify-center items-center gap-2">
+                <li>Home</li>
+                <li>About us</li>
+            </ul>
+        </nav>
+        <div class="py-6 md:py-10 flex justify-center items-start lg:items-center">
+            {@render children()}
+        </div>
+    </main>
+{/if}
