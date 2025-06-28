88cea8029096f9ca214f0588fe3132c0b6a00750
e-goat <gorgonaly@gmail.com>
2025-06-28 11:30:23 +0300

    Refactor svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
-
     kit: {
         adapter: adapter({
-            // see below for options that can be set here
+            runtime: 'nodejs18.x',
         }),
     },
 }
