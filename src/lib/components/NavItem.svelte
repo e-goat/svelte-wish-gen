315dad6315a159aaa35e7d38003f990dcc1d6899
e-goat <gorgonaly@gmail.com>
2025-09-01 00:31:16 +0300

    Update NavItemchange font size

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <a href={target}
-    class="text-2xl font-family-bebas text-custom-orange-600/90 hover:bg-white/20 hover:underline md:px-4 px-2 py-2 rounded-full transition-colors duration-200 {props.class}">{@render children?.()}</a>
+    class="text-xl font-family-roboto text-custom-orange-600/90 hover:bg-white/20 hover:underline md:px-4 px-2 py-2 rounded-full transition-colors duration-200 {props.class}">{@render children?.()}</a>
