2a9098608810fb739a469c1d5a9aa5f5cd190004
e-goat <gorgonaly@gmail.com>
2025-09-13 10:36:47 +0300

    remove google cloud credentials json import

--- src/lib/server/secrets.ts
+++ src/lib/server/secrets.ts
@@ -1,6 +1,6 @@
 import { env } from '$env/dynamic/private'
-import storageCredentials from '$lib/kartichkaqr-c3505b2bdbe7.json' assert { type: 'json' }
+// import storageCredentials from '$lib/kartichkaqr-c3505b2bdbe7.json' assert { type: 'json' }
 
 export const RESEND_API_KEY = env.RESEND_API_KEY
 
-export const GOOGLE_CLOUD_STORAGE_JSON = storageCredentials
+export const GOOGLE_CLOUD_STORAGE_JSON = {}
